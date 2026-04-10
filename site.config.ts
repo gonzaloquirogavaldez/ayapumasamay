5fba1c31c35ff34d2d53401d91d3018e62a755b1
Gonzalo Quiroga Valdez <gonqui19@gmail.com>
2026-04-10 14:30:48 -0300

    update proyecto

--- site.config.ts
+++ site.config.ts
@@ -1,5 +1,5 @@
-export const siteUrl = "https://mizar.majestico.co";
-export const siteTitle = "Mizar";
+export const siteUrl = "https://doblebe.com.ar";
+export const siteTitle = "DobleBe";
 export const defaultLocale = "es";
 export const locales = ["es", "en"];
 export type Locale = "es" | "en";
